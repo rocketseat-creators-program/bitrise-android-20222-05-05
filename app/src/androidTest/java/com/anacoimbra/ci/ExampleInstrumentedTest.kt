4be739ff9dc32044abd8cd84036b2ceb2bd50565
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2022-01-25 22:04:30 -0300

    fix: update instrumented test & setup text with app version

--- app/src/androidTest/java/com/anacoimbra/ci/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/anacoimbra/ci/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.anacoimbra.ci", appContext.packageName)
+        assertEquals(BuildConfig.APPLICATION_ID, appContext.packageName)
     }
 }
--- app/src/main/java/com/anacoimbra/ci/MainActivity.kt
+++ app/src/main/java/com/anacoimbra/ci/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
             ContinuousDeliveryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting(BuildConfig.VERSION_NAME)
                 }
             }
         }
@@ -25,14 +25,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(version: String) {
+    Text(text = "v $version")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ContinuousDeliveryTheme {
-        Greeting("Android")
+        Greeting(BuildConfig.VERSION_NAME)
     }
 }
